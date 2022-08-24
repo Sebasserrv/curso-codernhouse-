649229d480eab7139db39bacd5f2c27f35a27512
Sebasserrv <sebastiansvigil@gmail.com>
2022-08-24 19:31:44 -0400

    update

---
+++ clase1/index.js
@@ -0,0 +1,9 @@
+let nacimiento = prompt("ingresa tu fecha de nacimiento") 
+//! mostrara un cuadro de dialogo para ingresar un dato
+let anio = 2022
+const EDAD = anio - nacimiento
+const espa = " "
+
+ console.log(EDAD)
+ alert("tienes" + espa + EDAD + espa + "años") 
+ //! muestra una alerta que solo imprime strings
