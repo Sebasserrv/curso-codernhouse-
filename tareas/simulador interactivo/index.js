3e408773e3380ce934cd0ed332bd93fd62d61e83
Sebasserrv <sebastiansvigil@gmail.com>
2022-09-19 21:22:06 -0400

    update

--- tareas/simulador interactivo/index.js
+++
@@ -1,9 +0,0 @@
-const capital = parseFloat (prompt('ingrese monto del prestamo'))
-const cuota = parseFloat (prompt('ingrese cuota'))
-const interes = parseFloat(prompt('ingrese el interes sobre el prestamo'))
-
-if (condition) {
-    name
-} else {
-    
-}
--- tareas/simulador interactivo/usuario.js
+++ tareas/simulador interactivo/usuario.js
@@ -16,56 +16,40 @@ if (usario_creado == root, contraseña_creada == password) {
 
 function Usuario_Comun() {
     alert('Bienvenido '+ usario_creado)
-    
-    let valor = 1;
-    switch (valor) {
-      case 1:
-        console.log("Valor 1");
-        break;
-      case 2:
-        console.log("Valor 2");
-        break;
-      case 3:
-        console.log("Valor 3");
-        break;
-      default:
-        console.log("Valor Default")
-        break;
-    }
-    
-    let seleccionUsuario = parseInt(
+
+    // Variables para que el usuario elija 
+    let TrajeSleccionado = parseInt(
       prompt(
-        "Que desea comer ${s}: \n 1.Una Hamburguesa \n 2.Un Taco \n 3.Un Hot dog \n 4.Unos Nachos"
+        'Bien venid@ a Eva by Ara Battle \n Puede indicarnos que tipo de taje de baño quiere? \n 1. surfismo \n 2. para competencias \n 3. biquini dos piezas \n 4. una sola pieza '
       )
     );
-    if (seleccionUsuario === 1) {
-      alert("Usted selecciono una Hamburguesa");
-    } else if (seleccionUsuario === 2) {
-      alert("Usted selecciono un Taco");
-    } else if (seleccionUsuario === 3) {
-      alert("Usted selecciono un Hot dog");
-    } else if (seleccionUsuario === 4) {
-      alert("Usted selecciono un Hot dog");
-    }
+    // if (TrajeSleccionado === 1) {
+    //   alert("podras montar cualquier ola con este traje de baño surfista");
+    // } else if (TrajeSleccionado === 2) {
+    //   alert("Ya veras, con este traje de baño ganaras todas las competencias");
+    // } else if (TrajeSleccionado === 3) {
+    //   alert("Seguramente te llevaras todas las miradas een la playa (;");
+    // } else if (TrajeSleccionado === 4) {
+    //   alert("Un traje de baño a la ultima moda en camino");
+    // }
     
-    switch (seleccionUsuario) {
+    switch (TrajeSleccionado) {
       case 1:
-        alert("ya le traeran su Hamburguesa");
+        alert("podras montar cualquier ola con este traje de baño surfista");
         break;
       case 2:
-        alert("ya le traeran su Taco");
+        alert("Ya veras, con este traje de baño ganaras todas las competencias");
         break;
       case 3:
-        alert("ya le traeran su Hot dog");
+        alert("Seguramente te llevaras todas las miradas een la playa (;");
         break;
       case 4:
-        alert("ya le traeran su Hot dog");
+        alert("Un traje de baño a la ultima moda en camino");
         break;
       default:
         alert("Opción inválida");
         break;
-    }
-    
+    }    
 }
 
 function admin() {
